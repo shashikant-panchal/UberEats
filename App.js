9d8389d504fac0225e7a97de54402a3f881f82de
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-15 19:46:44 +0530

    Made a simple template for App component

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar backgroundColor={'lightblue'}/>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
