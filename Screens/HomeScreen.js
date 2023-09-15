11a554b815d5e5c99351d65b320e37d1fb4d0fea
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-15 21:31:24 +0530

    Updated

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import HeaderTabs from '../components/HeaderTabs'
+import SearchBox from '../components/SearchBox'
+import Categories from '../components/Categories'
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    return (
+        <SafeAreaView>
+            <View style={{ backgroundColor: 'white', padding: 15 }}>
+                <HeaderTabs />
+                <SearchBox />
+            </View>
+                <Categories />
+        </SafeAreaView>
+    )
 }
 
 export default HomeScreen
