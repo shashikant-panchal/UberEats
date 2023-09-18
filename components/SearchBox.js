532980a8df265e686d00d0149cb75cb59a48c8e5
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-18 19:08:08 +0530

    Completed SearchBox

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,44 +1,85 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import Icon from 'react-native-vector-icons/Ionicons'
-import AntIcon from 'react-native-vector-icons/AntDesign'
+import React, { useState } from 'react';
+import { View, TextInput, Text, TouchableOpacity, FlatList } from 'react-native';
+import MapboxGeocoder from '@mapbox/mapbox-sdk/services/geocoding';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const mapboxApiKey = 'pk.eyJ1IjoiaGFzZWViLWNyZWF0b3IiLCJhIjoiY2xtb215ZzlyMTVwNjJpczVvYmJrNno3byJ9.FVpLe8aS2Pd-igHOJSfEiA';
+
+const geocodingClient = MapboxGeocoder({ accessToken: mapboxApiKey });
 
 const SearchBox = () => {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState([]);
+    const [showSearchButton, setShowSearchButton] = useState(false);
+
+    const handleInputChange = (text) => {
+        setQuery(text);
+
+        if (text.length > 0) {
+            setShowSearchButton(true);
+        } else {
+            setShowSearchButton(false);
+        }
+
+        geocodingClient
+            .forwardGeocode({ query: text, limit: 5 })
+            .send()
+            .then(response => {
+                if (response && response.body && response.body.features) {
+                    setResults(response.body.features);
+                } else {
+                    setResults([]);
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                setResults([]);
+            });
+    };
+
+    const handleSelectPlace = (place) => {
+        console.log('Selected Place:', place);
+    };
+
+    const handleClearInput = () => {
+        setQuery('');
+        setShowSearchButton(false);
+        setResults([]);
+    };
+
     return (
-        <View style={{ marginTop: 15, flexDirection: 'row' }}>
-            <GooglePlacesAutocomplete
-                placeholder='Search'
-                styles={{
-                    textInput: {
-                        backgroundColor: '#E5E4E2',
-                        borderRadius: 20,
+        <View>
+            <View style={{ marginTop: 15, flexDirection: 'row', alignItems: 'center', backgroundColor: '#E5E4E2', borderRadius: 20, paddingLeft: 20 }}>
+                <Icon name="location-sharp" size={24} color={'black'} />
+                <TextInput
+                    placeholder='Search'
+                    style={{
+                        flex: 1,
+                        fontSize: 15,
+                        color: 'black',
                         fontWeight: '700',
-                        marginTop: 7,
-                    },
-                    textInputContainer: {
-                        backgroundColor: '#E5E4E2',
-                        borderRadius: 50,
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        marginRight: 10,
-                    }
-                }}
-                renderLeftButton={() => (
-                    <View style={{ marginLeft: 10 }}>
-                        <Icon name='location-sharp' size={24} color={'black'} />
-                    </View>
+                        height: 50,
+                    }}
+                    onChangeText={handleInputChange}
+                    value={query}
+                />
+                {showSearchButton && (
+                    <TouchableOpacity onPress={handleClearInput}>
+                        <Icon name='close-circle' size={20} color='black' style={{ marginRight: 10 }} />
+                    </TouchableOpacity>
                 )}
-                renderRightButton={() => (
-                    <View style={{ flexDirection: 'row', marginRight: 10, backgroundColor: 'white', padding: 9, borderRadius: 30, alignItems: 'center' }}>
-                        <AntIcon name='clockcircle' size={11} color={'black'} style={{ marginRight: 5 }} />
-                        <Text style={{ color: 'black', fontWeight: '700' }}>Search</Text>
-                    </View>
+            </View>
+            <FlatList
+                data={results}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => handleSelectPlace(item)} style={{ padding: 10 }}>
+                        <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 16 }}>{item.place_name}</Text>
+                    </TouchableOpacity>
                 )}
             />
         </View>
+    );
+};
 
-    )
-}
-
-export default SearchBox
+export default SearchBox;
