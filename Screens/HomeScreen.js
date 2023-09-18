5f5072d0e067ba6eb57feffe0267831fa8762acc
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-18 11:26:59 +0530

    Updated HomeScreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBox from '../components/SearchBox'
 import Categories from '../components/Categories'
+import RestaurentItem from '../components/RestaurentItem'
 
 const HomeScreen = () => {
     return (
@@ -11,9 +12,12 @@ const HomeScreen = () => {
                 <HeaderTabs />
                 <SearchBox />
             </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
                 <Categories />
+                <RestaurentItem />
+            </ScrollView>
         </SafeAreaView>
     )
 }
 
-export default HomeScreen
+export default HomeScreen;
