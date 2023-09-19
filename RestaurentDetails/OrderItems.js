64dfc396d76d067b0c308953e8c92b6b4d712058
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-19 13:49:49 +0530

    RestaurentDetails components added

---
+++ RestaurentDetails/OrderItems.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OrderItems = () => {
+  return (
+    <View>
+      <Text>OrderItems</Text>
+    </View>
+  )
+}
+
+export default OrderItems
