8683eda87434aca1e497b75d7b5044f91fc53073
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-19 13:50:13 +0530

    Added restaurentDetails Screen

---
+++ Screens/RestaurentDetails.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RestaurentDetails = () => {
+  return (
+    <View>
+      <Text>RestaurentDetails</Text>
+    </View>
+  )
+}
+
+export default RestaurentDetails
