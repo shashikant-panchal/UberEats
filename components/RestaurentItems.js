6a95e587d3c7db312f699f900851382c8e0950a6
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-19 13:18:11 +0530

    redesigned RestaurentItems UI

--- components/RestaurentItems.js
+++ components/RestaurentItems.js
@@ -1,121 +1,108 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export const localRestaurants = [
-    {
-        name: "Beachside Bar",
-        image_url:
-            "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
-        categories: ["Cafe", "Bar"],
-        price: "$$",
-        reviews: 1244,
-        rating: 4.5,
-    },
-    {
-        name: "Benihana",
-        image_url:
-            "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMgludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-        categories: ["Cafe", "Bar"],
-        price: "$$",
-        reviews: 1244,
-        rating: 3.7,
-    },
-    {
-        name: "India's Grill",
-        image_url:
-            "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMgludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-        categories: ["Indian", "Bar"],
-        price: "$$",
-        reviews: 700,
-        rating: 4.9,
-    },
-];
+const RestaurantItems = ({ restaurantData }) => {
+    const renderRestaurantItem = ({ item }) => {
+        return (
+            <TouchableOpacity activeOpacity={0.9} style={styles.itemContainer}>
+                <RestaurantImage image_url={item.image_url} />
+                <RestaurantInfo name={item.name} rating={item.rating} />
+            </TouchableOpacity>
+        );
+    };
 
-const RestaurantItems = (props) => {
     return (
-        <TouchableOpacity activeOpacity={1} style={{marginBottom: '71%', width:'95%', justifyContent:'center', alignSelf:'center' }}>
-            {props.restaurantData.map((restaurant, index) => (
-                <View
-                    key={index}
-                    style={{
-                        marginTop: 10,
-                        padding: 15,
-                        backgroundColor: 'white',
-                        borderRadius: 10,
-                        shadowColor: 'black',
-                        shadowOffset: { width: 0, height: 5 },
-                        shadowOpacity: 0.8,
-                        shadowRadius: 2,
-                        elevation: 10,
-                    }}>
-                    <RestaurantImage image_url={restaurant.image_url} />
-                    <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
-                </View>
-            ))}
-        </TouchableOpacity>
+        <FlatList
+            data={restaurantData}
+            renderItem={renderRestaurantItem}
+            keyExtractor={(item) => item.name}
+            contentContainerStyle={styles.container}
+        />
     );
-}
+};
 
-
-
-
-
-const RestaurantImage = (props) => {
+const RestaurantImage = ({ image_url }) => {
     return (
         <>
-            <Image source={{ uri: props.image_url }}
-                style={{
-                    width: '100%',
-                    height: 180,
-                    borderRadius: 10,
-                }}
+            <Image
+                source={{ uri: image_url }}
+                style={styles.image}
             />
-            <TouchableOpacity style={{
-                position: 'absolute',
-                right: 20,
-                top: 20
-            }}>
+            <TouchableOpacity style={styles.favoriteButton}>
                 <MaterialIcon name='heart-outline' size={25} color='white' />
             </TouchableOpacity>
         </>
     );
-}
+};
 
-const RestaurantInfo = (props) => {
+const RestaurantInfo = ({ name, rating }) => {
     return (
-        <View style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: 10
-        }}>
+        <View style={styles.infoContainer}>
             <View>
-                <Text style={{
-                    fontSize: 15,
-                    fontWeight: 'bold',
-                    color: 'black'
-                }}>{props.name}</Text>
-                <Text style={{
-                    fontSize: 13,
-                    color: 'gray'
-                }}>30-45 · min </Text>
+                <Text style={styles.nameText}>{name}</Text>
+                <Text style={styles.ratingText}>30-45 · min</Text>
             </View>
-            <View
-                style={{
-                    backgroundColor: 'lightgray',
-                    height: 30,
-                    width: 30,
-                    borderRadius: 15,
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}>
-                <Text style={{
-                    color: 'black'
-                }}>{props.rating}</Text>
+            <View style={styles.ratingContainer}>
+                <Text style={styles.rating}>{rating}</Text>
             </View>
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: '2.5%',
+        flex: 1
+    },
+    itemContainer: {
+        marginBottom: '3%',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.9,
+        shadowRadius: 2,
+        elevation: 10,
+    },
+    image: {
+        width: '100%',
+        height: 180,
+        borderRadius: 10,
+    },
+    favoriteButton: {
+        position: 'absolute',
+        right: 20,
+        top: 20,
+        backgroundColor: 'transparent',
+    },
+    infoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 10,
+        padding: 15,
+    },
+    nameText: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    ratingText: {
+        fontSize: 13,
+        color: 'gray',
+    },
+    ratingContainer: {
+        backgroundColor: 'lightgray',
+        height: 30,
+        width: 30,
+        borderRadius: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    rating: {
+        color: 'black',
+    },
+});
 
 export default RestaurantItems;
